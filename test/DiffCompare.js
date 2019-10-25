0aef2b4fc7836660261a613886df09946a69c80b
Clara Andrew-Wani <candrewwani@gmail.com>
2019-10-25 16:52:48 -0400

    Modify DiffCompare.js for use by Wikidiff2 and default diff engine
    Bug: T236333
    Change-Id: I3f852f714f5f15da51a8deb45f6d1f7ffb2ca909

--- test/DiffCompare.js
+++ test/DiffCompare.js
@@ -12,7 +12,7 @@ describe('Diff Compare with Variables', function () {
     });
 
     it('should edit a page', async () => {
-        const editPage = await wikiuser.edit(title, { text: 'Counting: \n*One' });
+        const editPage = await wikiuser.edit(title, { text: 'Counting: \n* One' });
 
         variables.revision1 = editPage.newrevid;
     });
@@ -36,14 +36,12 @@ describe('Diff Compare with Variables', function () {
     it('should compare revisions 1 and 4', async () => {
         const compareRevisions = await wikiuser.action('compare', { fromrev: variables.revision1, torev: variables.revision4 });
 
-        assert.match(compareRevisions.compare['*'],
-            /<td class='diff-addedline'><div><ins class=.*diffchange diffchange-inline.*\* Two<\/ins>/);
+        assert.match(compareRevisions.compare['*'], /<td class=.diff-addedline.>.*\* Two/);
     });
 
     it('should compare revisions 3 and 4', async () => {
         const compareRevisions = await wikiuser.action('compare', { fromrev: variables.revision3, torev: variables.revision4 });
 
-        assert.match(compareRevisions.compare['*'],
-            /<td class=.diff-deletedline.><div><del class=.*diffchange.*>\* Three<\/del>/);
+        assert.match(compareRevisions.compare['*'], /<td class=.diff-deletedline.>.*\* Three/);
     });
 });
