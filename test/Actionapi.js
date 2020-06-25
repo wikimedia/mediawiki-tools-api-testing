70bfec7639b8932bf27e93f224b821a450e6b6fd
daniel <dkinzler@wikimedia.org>
2020-06-25 13:56:30 +0200

    makeTag: wait for replication.
    Tags were sometimes not be usable immediately after creation due to replication
    lag, resulting in spurious "not allowed to be manually applied" errors.
    
    Change-Id: I53b4bd45087e57693f04f4b347be75c01c4fc318

--- test/Actionapi.js
+++ test/Actionapi.js
@@ -5,6 +5,11 @@ const { assert, action } = require('../index');
 describe('Action Api', () => {
 
     describe('Tag creation', () => {
+        let alice;
+
+        before(async () => {
+            alice = await action.alice();
+        });
 
         // If running this locally, the tag will already exist after the test has run once.
         it('Should create a new tag and return existing', async () => {
@@ -13,8 +18,7 @@ describe('Action Api', () => {
             const tag = await action.makeTag(tagName, `''${tagDisplay}''`);
             assert.deepEqual(tag, tagName);
 
-            const root = await action.root();
-            const tagList = await root.list('tags', { tglimit: 50, tgprop: 'displayname' });
+            const tagList = await alice.list('tags', { tglimit: 50, tgprop: 'displayname' });
             assert.deepInclude(tagList, { name: tagName, displayname: `<i>${tagDisplay}</i>` });
 
             // If tag has already been created, should still return because it has been cached
