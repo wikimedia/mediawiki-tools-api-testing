500ace7a6b71a744ee7e289d3ebf9054fbc58cb6
Arthur Taylor <arthur.taylor@wikimedia.de>
2024-07-02 11:39:46 +0200

    Add `force` option to `makeTag` action
    When we run tests in parallel, it can be the case that multiple
    tests call `makeTag` simultaneously. In this case there is a race
    condition in the `makeTag` logic - the check to see whether the
    tag already exists can return false, a parallel request can create
    the tag, and the attempt in the original process to create the
    tag then fails with 'tag-create-already-exists'.
    
    Add a `force` option to the `makeTag` action to allow "blind"
    creation of tags. The API request to create the tag will always
    be made regardless of whether it exists on the server, and the
    request will not return error in the event
    'tag-create-already-exists' is encountered. In this case, listing
    the tags from the server before executing the `managetags` API
    call will be skipped because we don't care about what's already
    on the server.
    
    Bug: T368903
    Change-Id: I786f3c308f513ea438eda7d4bacd164a7d7db37f

--- test/Actionapi.js
+++ test/Actionapi.js
@@ -13,8 +13,8 @@ describe('Action Api', () => {
 
 		// If running this locally, the tag will already exist after the test has run once.
 		it('Should create a new tag and return existing', async () => {
-			const tagName = 'api-test';
 			const tagDisplay = 'TestTagDisplay';
+			const tagName = 'api-test-create-new';
 			const tag = await action.makeTag(tagName, `''${ tagDisplay }''`);
 			assert.deepEqual(tag, tagName);
 
@@ -24,7 +24,35 @@ describe('Action Api', () => {
 			// If tag has already been created, should still return because it has been cached
 			const tag2 = await action.makeTag(tagName);
 			assert.deepEqual(tag2, tagName);
+			await action.deleteTag(tagName);
 		});
 
+		it('Should be idempotent with respect to new tag creation if force is true', async () => {
+			const tagDisplay = 'TestTagDisplay';
+			const tagName = 'api-test-create-new-with-existing';
+			let tag = await action.makeTag(tagName, `''${ tagDisplay }''`);
+			assert.deepEqual(tag, tagName);
+
+			/* Set the 'force=true' option so that we try the create even if the tag exists.
+			 * This test will pass whether or not the flag is set - making a test that requires
+			 * the flag would mean recreating the race condition inside the test suite - maybe
+			 * possible by mocking the API? But setting to `true` here at least exercises the
+			 * new codepath */
+			tag = await action.makeTag(tagName, `''${ tagDisplay }''`, true);
+			assert.deepEqual(tag, tagName);
+
+			const tagList = await alice.list('tags', { tglimit: 50, tgprop: 'displayname' });
+			assert.deepInclude(tagList, { name: tagName, displayname: `<i>${ tagDisplay }</i>` });
+
+			// If tag has already been created, should still return because it has been cached
+			const tag2 = await action.makeTag(tagName);
+			assert.deepEqual(tag2, tagName);
+			await action.deleteTag(tagName);
+		});
+
+		it('Should delete a non-existent tag without complaining', async () => {
+			const result = await action.deleteTag('SomeNonexistentTag');
+			assert.deepEqual(result.error.code, 'tags-delete-not-found');
+		});
 	});
 });
